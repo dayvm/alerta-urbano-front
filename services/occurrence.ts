8d807b3ebcce1dc2907cb3214b84803832539e9d
David Mendes <150140823+daavidmendes@users.noreply.github.com>
2025-12-02 09:32:30 +0000

    Implementando hook e conexao de um report

--- services/occurrence.ts
+++ services/occurrence.ts
@@ -8,6 +8,7 @@ export interface Occurrence {
   longitude: number;
   addressText: string;
   currentStatus: "OPEN" | "IN_PROGRESS" | "RESOLVED";
+  createdAt: string; // <--- ADICIONE ESTA LINHA
   categoryName: string;
   authorName: string;            // Adicionei baseado no seu JSON
   responsibleInstitutionName?: string; // Adicionei (pode ser null)
@@ -17,5 +18,9 @@ export const occurrenceService = {
   getAll: async () => {
     const { data } = await api.get<Occurrence[]>("/occurrences");
     return data;
-  }
+  },
+  getById: async (id: number) => {
+    const { data } = await api.get<Occurrence>(`/occurrences/${id}`);
+    return data;
+  },
 };
